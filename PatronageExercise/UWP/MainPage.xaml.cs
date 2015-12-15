2234985c81994a99753d484c2f716c8333a8d7d0
bobista0 <borys.rybak@gmail.com>
2015-12-15 01:40:53 +0100

    add app bar and simple bool property for opening or closing app bar

--- PatronageExercise/UWP/MainPage.xaml.cs
+++ PatronageExercise/UWP/MainPage.xaml.cs
@@ -4,9 +4,27 @@ namespace UWP
 {
     public sealed partial class MainPage : Page
     {
+        #region PRIVATE FIELDS
+        #endregion
+
+        #region CONSTRUCTORS
         public MainPage()
         {
             this.InitializeComponent();
         }
+        #endregion
+
+        #region PROPERTIES
+        private bool _isGalleryEmpty = true;
+        public bool IsGalleryEmpty
+        {
+            get { return _isGalleryEmpty; }
+            set { _isGalleryEmpty = value; }
+        }
+        #endregion
+
+
+        #region METHODS
+        #endregion
     }
 }
