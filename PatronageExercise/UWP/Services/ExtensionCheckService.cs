206380f2c09d63c7c13e3ae30eddf077895b5c86
bobista0 <borys.rybak@gmail.com>
2015-12-17 02:38:13 +0100

    implement extension-check service

---
+++ PatronageExercise/UWP/Services/ExtensionCheckService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Windows.Storage;
+
+namespace UWP.Services
+{
+    public sealed class ExtensionCheckService
+    {
+        #region LAZY-SINGLETON IMPLEMENTATION
+        private static readonly Lazy<ExtensionCheckService> lazy = new Lazy<ExtensionCheckService>(() => new ExtensionCheckService());
+        public static ExtensionCheckService Instance
+        {
+            get { return lazy.Value; }
+        }
+        private ExtensionCheckService() { }
+        #endregion
+
+        #region METHODS
+        public bool HasPhotoExtension(IStorageItem item)
+        {
+            return HasSpecificExtension(item, ".jpg", ".jpeg", ".png", ".tif", ".bmp");
+        }
+
+        public bool HasSpecificExtension(IStorageItem item, params string[] extensions)
+        {
+            var result = false;
+
+            foreach (var extension in extensions)
+            {
+                if (result = Path.GetExtension(item.Name).Equals(extension, StringComparison.CurrentCultureIgnoreCase))
+                    break;
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
