87b9ba6b8be6532f617fbc46f960c3d248eb0125
bobista0 <borys.rybak@gmail.com>
2016-01-03 20:18:54 +0100

    add page resources

--- PatronageExercise/UWP/Views/DetailPage.xaml.cs
+++ PatronageExercise/UWP/Views/DetailPage.xaml.cs
@@ -20,9 +20,9 @@ namespace UWP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class GalleryPage : Page
+    public sealed partial class DetailPage : Page
     {
-        public GalleryPage()
+        public DetailPage()
         {
             this.InitializeComponent();
         }
