386ee1488b5ff28f12e7d0b670b77f005daf5e62
bobista0 <borys.rybak@gmail.com>
2016-01-05 00:03:32 +0100

    add some methods for gallery display

--- PatronageExercise/UWP/Views/GalleryPage.xaml.cs
+++ PatronageExercise/UWP/Views/GalleryPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UWP.Models;
+using UWP.Services;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,11 +23,51 @@ namespace UWP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class GalleryPage : Page
+    public sealed partial class GalleryPage : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public GalleryPage()
         {
             this.InitializeComponent();
+
+            DisplayGallery();
+        }
+
+        private List<Photo> photoCollection;
+        public List<Photo> PhotoCollection
+        {
+            get { return photoCollection; }
+            set
+            {
+                if (value != photoCollection)
+                {
+                    photoCollection = value;
+                    OnPropertyChanged("PhotoCollection");
+                }
+            }
+        }
+
+        private async void DisplayGallery()
+        {
+            var gallery = await PhotoCameraService.Instance.LoadAndGetGallery();
+
+            if (gallery != null)
+                PhotoCollection = gallery;
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void OnGridViewItemClick(object sender, ItemClickEventArgs e)
+        {
+
         }
     }
 }
