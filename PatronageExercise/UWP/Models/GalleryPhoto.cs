f2dafdc5ecb00bec4a4a246b3dd7830ee7aae990
bobista0 <borys.rybak@gmail.com>
2016-01-05 20:40:39 +0100

    add another model for miniature gallery photos

---
+++ PatronageExercise/UWP/Models/GalleryPhoto.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Media.Imaging;
+
+namespace UWP.Models
+{
+    public class GalleryPhoto
+    {
+        public uint Index { get; set; }
+        public string Name { get; set; }
+        public BitmapImage Thumbnail { get; set; }
+    }
+}
